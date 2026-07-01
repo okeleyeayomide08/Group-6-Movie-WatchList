b2c8b22a7f0eb6bbd36335bf5c71564b1b80ce82
cnwhytenwamah <cnwhytenwamah@gmail.com>
2026-07-02 00:20:07 +0100

    template for crafting service layer for movie watchlist

---
+++ services/movieService.js
@@ -0,0 +1,23 @@
+export async function getAllMovies(){
+    // Implementation for fetching all movies
+}
+
+export async function getMovieById(id){
+    // Implementation for fetching a movie by its ID
+}
+
+export async function createMovie(movieData){
+    // Implementation for creating a new movie
+}
+
+export async function updateMovie(id, movieData){
+    // Implementation for updating an existing movie
+}
+
+export async function deleteMovie(id){
+    // Implementation for deleting a movie by its ID
+}
+
+export async function getUnWatchedMovies(){
+    // Implementation for fetching all unwatched movies
+}
