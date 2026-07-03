f99c2aab1d852f925ee1924b90d3f227647bad07
cnwhytenwamah <cnwhytenwamah@gmail.com>
2026-07-03 15:17:09 +0100

    added crud logic in the service layer

--- services/movieService.js
+++ services/movieService.js
@@ -1,23 +1,64 @@
-export async function getAllMovies(){
-    // Implementation for fetching all movies
+import crypto from 'crypto';
+import { readMovies, writeMovies } from "./movie.storage.js";
+
+export async function getAllMovies() {
+  return await readMovies();
 }
 
-export async function getMovieById(id){
-    // Implementation for fetching a movie by its ID
+export async function getMovieById(id) {
+  const movies = await readMovies();
+
+  return movies.find((movie) => movie.id === id);
 }
 
-export async function createMovie(movieData){
-    // Implementation for creating a new movie
+export async function createMovie(movie) {
+  const movies = await readMovies();
+
+  const now = new Date().toISOString();
+  const newMovie = {
+    id: crypto.randomUUID(),
+    title: movie.title,
+    director: movie.director,
+    year: movie.year,
+    genre: movie.genre,
+    watched: movie.watched,
+    createdAt: now,
+    updatedAt: now,
+  };
+
+  movies.push(newMovie);
+  await writeMovies(movies);
+  return newMovie;
 }
 
-export async function updateMovie(id, movieData){
-    // Implementation for updating an existing movie
+export async function updateMovie(id, movie) {
+  const movies = await readMovies();
+  const index = movies.findIndex((m) => m.id === id);
+  if (index === -1) {
+    return null;
+  }
+  movies[index] = {
+    ...movies[index],
+    ...movie,
+    updatedAt: new Date().toISOString(),
+  };
+  await writeMovies(movies);
+  return movies[index];
 }
 
-export async function deleteMovie(id){
-    // Implementation for deleting a movie by its ID
+export async function deleteMovie(id) {
+  const movies = await readMovies();
+  const index = movies.findIndex((movie) => movie.id === id);
+  if (index === -1) {
+    return false;
+  }
+  movies.splice(index, 1);
+  await writeMovies(movies);
+  return true;
 }
 
-export async function getUnWatchedMovies(){
-    // Implementation for fetching all unwatched movies
+export async function getUnwatchedMovies() {
+  const movies = await readMovies();
+
+  return movies.filter((movie) => movie.watched === false);
 }
